1e922a3f55cf16db665e83c958a697bae665e8d9
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2020-03-30 19:17:41 +0200

    no message

--- Sources/RuntimeTestCase/RuntimeTestCaseClass.swift
+++ Sources/RuntimeTestCase/RuntimeTestCaseClass.swift
@@ -5,7 +5,7 @@ open class RuntimeTestCaseClass: XCTestCase {
         if let runtimeTestCaseClass = Self.self as? RuntimeTestCaseProtocol0.Type {
             return runtimeTestCaseClass.runtimeTestSuite
         } else {
-            fatalError("The test suite for generating runtime tests could not be found")
+            return super.defaultTestSuite
         }
     }
 }
