27a1a9c5d8c6c46f02a14cd814b071e2daf76d98
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2020-03-30 18:00:44 +0200

    no message

--- Sources/RuntimeTestCase/RuntimeTestCaseClass.swift
+++ Sources/RuntimeTestCase/RuntimeTestCaseClass.swift
@@ -1,7 +1,7 @@
 import XCTest
 
-public class RuntimeTestCaseClass: XCTestCase {
-    override public class var defaultTestSuite: XCTestSuite {
+open class RuntimeTestCaseClass: XCTestCase {
+    override open class var defaultTestSuite: XCTestSuite {
         if let runtimeTestCaseClass = Self.self as? RuntimeTestCaseProtocol0.Type {
             return runtimeTestCaseClass.runtimeTestSuite
         } else {
--- Sources/RuntimeTestCase/RuntimeTestCaseSpec.swift
+++ Sources/RuntimeTestCase/RuntimeTestCaseSpec.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 public struct RuntimeTestCaseSpec<T> {
-    let name: String
-    let setup: (T) -> Void
+    public let name: String
+    public let setup: (T) -> Void
 
-    init(_ name: String, _ setup: @escaping (T) -> Void = { _ in }) {
+    public init(_ name: String, _ setup: @escaping (T) -> Void = { _ in }) {
         self.name = name
         self.setup = setup
     }
